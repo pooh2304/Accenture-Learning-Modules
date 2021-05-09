2f3d51c905ae76900e5ae38b54a09ac7e74d628f
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-10 01:19:38 +0530

    Codes

--- Algorithm Analysis and Design Concepts/Hands-On/String Sorting.java
+++ Algorithm Analysis and Design Concepts/Hands-On/String Sorting.java
@@ -1,3 +1,32 @@
+/*
+
+John likes to play with string and he always tries to do something new with it. This time he found a very typical way to sort a string. What he did is that he took a string 
+composed of both lowercase and uppercase letters and sorted it in such a manner such that the uppercase and lowercase letter comes in an alternate manner but in sorted way.  
+He needs your help to complete this job as fast as possible.
+
+Example:
+Input: bAwutndekWEdkd
+Output: AbEdWddekkntuw
+
+Explanation:
+Here we can see that letter ‘A’,’E’,’W’ are sorted as well as letters “b,d,d,d,e,k,k,n,t,u,w” are sorted but both appears alternately in the string as far as possible.
+
+Note: Create main() inside the class 'SortDriver'
+
+Input:
+The first line of input contains the string to be sorted. 
+
+Output:
+Print the string in required sorted format.
+
+Sample Input:
+Enter the String: HelLoWoRld
+
+Sample Output:
+The Sorted String is: HdLeRlWloo
+
+*/
+
 import java.util.Scanner;
 import java.lang.*;
 
