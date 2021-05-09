7df0852764f24e3fcecb32a73772829c2e0d2fa7
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-09 21:48:21 +0530

    Codes

--- Algorithm Analysis and Design Concepts/Hands-On/String Reverse - Recursion.java
+++ Algorithm Analysis and Design Concepts/Hands-On/String Reverse - Recursion.java
@@ -1,3 +1,26 @@
+/*
+
+Ram participates in an online gaming contest, where he is asked to code logic in Java to reverse the given word using recursion. Help Ram to perform the same. Check the 
+sample input and output statements for more clarifications.
+
+Function signature should be: public static String reverseString(String str)
+
+Implement the main() inside the class 'ReverseDriver'
+
+Sample Input/Output 1:
+Enter the string: teknoturf
+Reversed string is: frutonket
+
+Sample Input/Output 2:
+Enter the string: krishna
+Reversed string is: anhsirK
+
+Sample Input/Output3:
+Enter the string: Hello World
+Reversed string is: dlroW olleH
+
+*/
+
 import java.util.Scanner;
 
 class ReverseDriver {
