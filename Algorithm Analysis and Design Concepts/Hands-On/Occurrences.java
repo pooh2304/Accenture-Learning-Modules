e1dc6f506af77c4316784c5df63e349a1a99bfcd
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-09 18:48:07 +0530

    Codes

--- Algorithm Analysis and Design Concepts/Hands-On/Occurrences.java
+++ Algorithm Analysis and Design Concepts/Hands-On/Occurrences.java
@@ -34,7 +34,7 @@ class Node {
 
 public class DeleteMain {
     Node head;
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         DeleteMain obj = new DeleteMain();
         int n = sc.nextInt();
