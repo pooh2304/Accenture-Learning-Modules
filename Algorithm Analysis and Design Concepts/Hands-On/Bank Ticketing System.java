fda557f216f4ed7ba7fac98c30bec3cabb2728de
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-09 18:56:16 +0530

    Codes

--- Algorithm Analysis and Design Concepts/Hands-On/Bank Ticketing System.java
+++ Algorithm Analysis and Design Concepts/Hands-On/Bank Ticketing System.java
@@ -1,3 +1,56 @@
+/*
+
+In the ticketing system of a Bank, each customer takes a ticket and is served when their number is called. Imagine there are five customers take a ticket from our ticketing 
+system. The first customer has a ticket displaying the number 1 and the fifth customer has a ticket displaying the number 5. The customers who take the ticket should 
+maintain a queue and the customer with the first ticket should be served first. When the first customer is served and that ticket is removed from the queue.
+
+Implement this ticket system using appropriate data structure.
+
+Note:
+1. There is no limitation for the customers to take tickets.
+2. Create a class called 'Ticket' to implement the main()
+
+Sample Input Output 1: [Values given in bold represents the input]
+Enter the number of customer takes the tickets:
+3
+
+The tickets in the system are:
+1 2 3 
+
+Enter the number of tickets served:
+2
+
+The served tickets are:
+1 2 
+The unserved tickets are:
+3 
+
+Note: In the above sample the input provided by the user are 3, 2 which represents "number of customer takes the tickets" and "number of tickets served".
+
+
+Sample Input Output 2:
+Enter the number of customer takes the tickets:
+5
+
+The tickets in the system are:
+1 2 3 4 5 
+
+Enter the number of tickets served:
+5
+
+The served tickets are:
+1 2 3 4 5 
+No more tickets to be served
+
+
+Sample Input Output 3:
+Enter the number of customer takes the tickets:
+-3
+
+Invalid Number
+
+*/
+
 import java.util.*;
 
 public class Ticket {
