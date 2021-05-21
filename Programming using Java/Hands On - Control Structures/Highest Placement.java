07b5a5ce72be3bf4377e0525572469a975bba268
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-22 05:00:34 +0530

    Codes

--- Programming using Java/Hands On - Control Structures/Highest Placement.java
+++ Programming using Java/Hands On - Control Structures/Highest Placement.java
@@ -1,6 +1,6 @@
 /*
 
-SRV college wants to recognize the department which has succeeded in getting the maximum number of placements for this academic year. The departments that have participated 
+SRV college wants to recognize the department which has succeeded in getting the maximum number of placements for this academic year. The departments that have participated
 in the recruitment drive are CSE,ECE, MECH. Help the college find the department getting maximum placements. Check for all the possible output given in the sample snapshot.
 Note : 
     If any input is negative, the output should be "Input is invalid".  
