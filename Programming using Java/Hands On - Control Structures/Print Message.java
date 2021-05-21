4ad3f46b69e0b035aaeaff548475dc335cf2e06a
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-21 23:35:35 +0530

    Codes

---
+++ Programming using Java/Hands On - Control Structures/Print Message.java
@@ -0,0 +1,14 @@
+/*
+
+Write a progam to display a message "Welcome to object oriented programming"
+
+Sample Output 1:
+Welcome to object oriented programming
+
+*/
+
+class PrintMessage {
+    public static void main (String[] args) {
+        System.out.println("Welcome to object oriented programming");
+    }
+}
