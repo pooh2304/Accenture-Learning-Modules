6f9298a37646ab0a992e871d63571c106c29700b
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-24 04:47:46 +0530

    Codes

--- Programming using Java/Hands On - Arrays/Make a number.java
+++ Programming using Java/Hands On - Arrays/Make a number.java
@@ -1,8 +1,9 @@
 /*
 
-Sujan gets n numbers in an array.  Write a Java program to take the single digit even numbers in the array and make a number by combining those even numbers alone.
+Sujan gets n numbers in an array. Write a Java program to take the single digit even numbers in the array and make a number by combining those even numbers alone.
 
-Example: If the array is {2,7,14,24,41,4} the output should be a single number 24, if not found print "Single digit even number is not found in the array".If the array size is zero or lesser then display the message as "Invalid array size"
+Example: If the array is {2,7,14,24,41,4} the output should be a single number 24, if not found print "Single digit even number is not found in the array".
+If the array size is zero or lesser then display the message as "Invalid array size"
 
 Sample Input 1:
 Enter the size of an array:
