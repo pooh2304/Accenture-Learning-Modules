deef52214c29e85417fb927c48fa6af7658c6678
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-23 19:48:38 +0530

    Codes

---
+++ Programming using Java/Running Case Study/State Board of Cricket Council –V2.0/UserInterface.java
@@ -0,0 +1,38 @@
+package com.sbcc.main;
+
+import com.sbcc.model.*;
+import com.sbcc.utility.*;
+import java.util.*;
+import java.lang.*;
+import com.sbcc.skeletonvalidator.SkeletonValidator;
+
+public class UserInterface {
+    public static Player pl;
+
+	public static void main(String[] args) {
+		SkeletonValidator validator = new SkeletonValidator();
+		Scanner sc = new Scanner(System.in);
+		int choice=0;
+        while(choice!=2){
+            System.out.println("1.Parse the player details and create player\n2.Exit");
+            choice=sc.nextInt();
+            switch(choice){
+            case 1:
+                System.out.println("Enter the player details");
+                String inp= sc.next();
+                Player obj;
+                SBCCUtility obj1= new SBCCUtility();
+                obj=obj1.parsePlayerDetails(inp);
+                System.out.println("Player id: "+obj.getPlayerId());
+                System.out.println("Player name: "+obj.getPlayerName());
+                System.out.println("Matches played: "+obj.getMatchesPlayed());
+                System.out.println("Total runs scored: "+obj.getRunScored());
+                System.out.println("Playing zone: "+obj.getPlayingZone());
+                break;
+            case 2:
+                System.out.println("Thank you for using SBCC application");
+                System.exit(0);
+            }
+        }
+	}
+}
