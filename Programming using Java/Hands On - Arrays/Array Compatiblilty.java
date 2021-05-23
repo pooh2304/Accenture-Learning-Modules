3caa61c289b3fb45c260fd764103a6da7320936f
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-24 04:43:30 +0530

    Codes

--- Programming using Java/Hands On - Arrays/Array Compatiblilty.java
+++ Programming using Java/Hands On - Arrays/Array Compatiblilty.java
@@ -1,6 +1,8 @@
 /*
 
-Two arrays are said to be compatible if they are of the same size and if the ith element in the first array is greater than or equal to the ith element in the second array for all i elements.If the array size is zero or lesser then display the message "Invalid array size".Write a Java program to find whether 2 arrays are compatible or not.If the arrays are compatible display the message as "Arrays are Compatible" ,if not then display the message as "Arrays are Not Compatible".
+Two arrays are said to be compatible if they are of the same size and if the ith element in the first array is greater than or equal to the ith element in the second array
+for all i elements.If the array size is zero or lesser then display the message "Invalid array size".Write a Java program to find whether 2 arrays are compatible or not.
+If the arrays are compatible display the message as "Arrays are Compatible" ,if not then display the message as "Arrays are Not Compatible".
 
 Sample Input 1:
 Enter the size for First array:
