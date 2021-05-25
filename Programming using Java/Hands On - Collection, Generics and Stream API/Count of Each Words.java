18b062cd98f4a5160bf4bffeae51ff7b973ad316
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-26 03:09:49 +0530

    Codes

--- Programming using Java/Hands On - Collection, Generics and Stream API/Count of Each Words.java
+++ Programming using Java/Hands On - Collection, Generics and Stream API/Count of Each Words.java
@@ -1,3 +1,40 @@
+/*
+
+Miss.Jane, an experienced English professor, gives practice tests to her students to improve their written skills. Everyday students write an article and they submit it 
+to Jane. Jane is particular that the students use only special characters like , ; : . ? ! in the article.
+
+[Note : Using the above mentioned special characters will help to split the words in a sentence. All other special characters when used will be considered as a part of 
+the word itself. ]
+
+She counts the total number of words used and the count of each word in the article. Based on this analysis she gives her feedback to the students.  
+
+Difficulty arises when the number of students increase. So she wanted to automate the process in the following format. Help her to write a java program to display the 
+words and the number of times it has been used in the article and to display the words using lower case and in alphabetical order.
+
+Sample Input 1 :
+hello Hello HEllo hi hi: hi! Welcome,   welcome
+
+Sample Output 1 :
+Number of words 8
+Words with the count
+hello: 3
+hi: 3
+welcome: 2
+
+
+Sample Input 2 :
+Use Arrays.sort method
+
+Sample Output 2:
+Number of words 4
+Words with the count
+use: 1
+arrays: 1
+sort: 1
+method: 1
+
+*/
+
 import java.util.*;
 
 @SuppressWarnings("unchecked")
