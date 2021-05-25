37e04742ca9668488852e63ca022b9e7b10229b8
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-25 21:56:32 +0530

    Codes

--- Programming using Java/Hands On - Exception Handling/Get Input and handle exception.java
+++ Programming using Java/Hands On - Exception Handling/Get Input and handle exception.java
@@ -1,3 +1,28 @@
+/*
+
+Get a number as input from the user using nextInt method of  Scanner.
+If the input provided by the user is not a number (contains alphabets or special characters), it will throw an Exception, InputMismatchException.
+
+When the input provided by the user is correct, print the number, else handle it using catch block for InputMismatchException and print the message as 
+“Input should be a number”.
+
+Note :  InputMismatchException is available in java.util package
+
+Sample Input 1 :
+45
+
+Sample Output 1 :
+The input is 45
+
+
+Sample Input 2 :
+abc45
+
+Sample Output 1 :
+Input should be a number
+
+*/
+
 import java.util.*;
 
 public class Main {
