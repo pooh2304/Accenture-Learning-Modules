5896031ce36da2bf8a69ad2a368998be953e9de2
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-25 22:11:02 +0530

    Codes

--- Programming using Java/Hands On - Exception Handling/Validate Date using ParseException - Use try catch throws.java
+++ Programming using Java/Hands On - Exception Handling/Validate Date using ParseException - Use try catch throws.java
@@ -1,3 +1,38 @@
+/*
+
+Get  the input for date of birth as String from the user.  Check whether the given date is valid using SimpleDateFormat.
+
+Write a java program to meet the above requirement.
+
+Implement throws clause in this requirement.
+
+In the Main class implement the below method:
+	public static Date convertStringToDate(String str) – This method should convert the String str in the format dd/MM/yyyy to java.util.Date using SimpleDateFormat.  
+							     This method should throw ParseException. If valid return the converted Date object.
+
+Note: When using SimpleDateFormat to convert String to Date it will allow the code to compile only if you handle the exception “ParseException” available in java.text package.
+
+Now try to recollect what type of Exception is ParseException – Checked or unchecked ?
+
+In the main method, get the date as String in the format dd/MM/yyyy and invoke the method convertStringToDate.  If date is valid display “<String> is a valid date”.
+
+Else handle the Exception thrown and print “<String> is not a valid date”.
+
+Sample Input 1 :
+31/10/2020
+
+Sample Output 1:
+31/10/2020 is a valid date
+
+
+Sample Input 2 :
+31/11/2020
+
+Sample Output 2:
+31/11/2020 is not  a valid date
+
+*/
+
 import java.util.*;
 import java.text.ParseException;
 import java.text.DateFormat;
