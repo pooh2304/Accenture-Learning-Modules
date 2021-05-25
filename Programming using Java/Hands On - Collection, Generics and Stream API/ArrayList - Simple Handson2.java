b403c507794fe5be13352aec8a617c89c0369650
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-25 23:57:48 +0530

    Codes

---
+++ Programming using Java/Hands On - Collection, Generics and Stream API/ArrayList - Simple Handson2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class UserInterface {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of elements to add");
+		int elements = sc.nextInt();
+		sc.nextLine();
+		ArrayList<String> fruits = new ArrayList<String>();
+		for(int i=0;i<elements;i++){
+			String fruit = sc.nextLine();
+			fruits.add(fruit);
+		}
+		Iterator<String> itr = fruits.iterator();
+		while(itr.hasNext()){
+		    System.out.println(itr.next());
+		}
+	}
+}
