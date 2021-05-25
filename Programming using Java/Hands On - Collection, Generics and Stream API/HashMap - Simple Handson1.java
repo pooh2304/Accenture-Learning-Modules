ddf07ffaa6ed2ca23d276c55119ed4e82c599f4e
Pooja Bharti <51374683+pooh2304@users.noreply.github.com>
2021-05-26 01:06:53 +0530

    Codes

--- Programming using Java/Hands On - Collection, Generics and Stream API/HashMap - Simple Handson1.java
+++ Programming using Java/Hands On - Collection, Generics and Stream API/HashMap - Simple Handson1.java
@@ -1,3 +1,33 @@
+/*
+
+Write a Java program to associate the specified value, country name (String as Value) with the specified key, country code (Integer as key) in a Hash Map and print out 
+the collection.
+
+Note: Import only required classes.
+
+Sample Input / Output:
+Enter number of elements to add
+4
+Enter the country code
+91
+Enter the country name
+India
+Enter the country code
+973
+Enter the country name
+Bahrain
+Enter the country code
+1
+Enter the country name
+Canada
+Enter the country code
+52
+Enter the country name
+Mexico
+{1=Canada, 52=Mexico, 91=India, 973=Bahrain}
+
+*/
+
 import java.util.*;
 
 public class UserInterface {
